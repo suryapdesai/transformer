de26f76aa62a8a0fa54a97147510d68bb745a8b4
suryapdesai <surya@plivo.com>
2018-01-30 11:56:55 +0530

    updating setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,7 @@ setup(
     url="https://github.com/zapier/transformer",
     classifiers=[
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python'
     ],
     install_requires=[
         'arrow==0.7.0',
